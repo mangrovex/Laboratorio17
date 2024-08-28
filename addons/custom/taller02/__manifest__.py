64a989ee1a0bfb6999790d51f4dd3db2bb75cbd2
mevu <manuel.vega@manexware.com>
2024-08-27 22:26:58 -0500

    clase 3

--- addons/custom/taller02/__manifest__.py
+++ addons/custom/taller02/__manifest__.py
@@ -1,34 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "taller02",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'name': "Taller02",
+    'summary': "Nursery Plants",
+    'description': """ Nursery Plants """,
+    'author': "Manexware S.A.",
+    'website': "https://www.manexware.com",
     'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'version': '17.0.0.0.1',
+    'depends': ['base','web','website','portal'],
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/nursery_plants_views.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
 }
-
